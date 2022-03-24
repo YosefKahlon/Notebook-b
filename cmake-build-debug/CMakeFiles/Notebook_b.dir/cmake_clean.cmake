e184bd34b60aad3928ee23180f2a9c92303aa665
yosefkahlon <yosefkahlon53@gmail.com>
2022-03-24 19:34:40 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Notebook_b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Notebook_b.dir/main.cpp.o"
+  "Notebook_b"
+  "Notebook_b.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Notebook_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
